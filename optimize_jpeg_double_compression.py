505730436d85304acf7267763120e0b3ad64f5ee
Govind Mittal <gm2724@gv03.nyu.cluster>
2021-03-10 13:08:05 -0500

    add hpc paths 	modified:   optimize_jpeg_double_compression.py

--- optimize_jpeg_double_compression.py
+++ optimize_jpeg_double_compression.py
@@ -23,11 +23,13 @@ from helpers.tf_helpers import activation_mapping
 
 v_images = 256
 t_images = 1024
-batch_size = 64
+batch_size = 256
 patch_size = 64
-epochs = 15
-data_dir = "/home/govind/Workspace/neural-imaging-dev/data/rgb/native12k"
-save_dir = "./outputs"
+epochs = 1500
+data_dir = "/scratch/gm2724/data/rgb/native12k"
+save_dir = "/scratch/gm2724/nip_runs/hyperopt"
+#data_dir = "/home/govind/Workspace/neural-imaging-dev/data/rgb/native12k"
+#save_dir = "./outputs"
 
 if True:
     physical_devices = tf.config.list_physical_devices("GPU")
