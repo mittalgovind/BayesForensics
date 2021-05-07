cc1594f8a4828b65244bb752ed89860583c57342
Marcelo Sandoval <jms1595@nyu.edu>
2021-05-07 08:49:12 -0400

    Minor/irrelevant

--- workflows/bayes_scaling_factor/__init__.py
+++ workflows/bayes_scaling_factor/__init__.py
@@ -12,5 +12,5 @@
 
 from .train import preprocess_batch, train_single, train_ensemble
 from .validate import run_tests
-from .flow import SFP, BayarStammSFP, BayarStammCalibrated
+from .flow import SFP, BayarStammSFP # , BayarStammCalibrated
 from .visualize import sf_plot
