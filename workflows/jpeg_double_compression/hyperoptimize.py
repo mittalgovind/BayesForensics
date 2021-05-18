f3cda4e06ba16b443a7415eeb276fb6b8f1a2fb7
mittalgovind <mittal@nyu.edu>
2021-05-18 18:34:04 -0400

    fix args

--- workflows/jpeg_double_compression/hyperoptimize.py
+++ workflows/jpeg_double_compression/hyperoptimize.py
@@ -192,7 +192,7 @@ if __name__ == "__main__":
         parser = argparse.ArgumentParser(description="Hyperopt")
         parser.add_argument("--gpus", default=1, type=int)
         parser.add_argument("--cpus", default=2, type=int)
-        parser.add_argument("--epochs", default=5, type=int)
+        parser.add_argument("--epochs", default=6, type=int)
         parser.add_argument("--bs", default=2048, type=int)
         parser.add_argument("--num-samples", default=250, type=int)
         parser.add_argument("--n-images", default=512, type=int)
