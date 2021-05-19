4fb3272fe69b1935ea72afb477bd5e2e064a1d04
Marcelo Sandoval-Castañeda <marcelo.sc@nyu.edu>
2021-05-20 00:36:37 +0400

    Track shapes

--- workflows/jpeg_double_compression/validate.py
+++ workflows/jpeg_double_compression/validate.py
@@ -67,7 +67,7 @@ def validate(model, data, batch_size, cache, num_runs):
                     logits = tf.convert_to_tensor([model(images, training=False) for _ in range(len(num_runs))])
 
                 predictions = get_pred(logits)
-                print(predictions)
+                print(predictions.shape, labels.shape)
             accuracies[qf1_ind, qf2_ind] += np.sum(predictions == labels)
 
             print(accuracies[qf1_ind, qf2_ind])
