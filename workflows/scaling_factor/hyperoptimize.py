b8a30a21c1064e9ee98204a2a70d6df2d9491bef
mittalgovind <mittal@nyu.edu>
2021-05-20 07:42:16 -0400

    limit concurrency

--- workflows/scaling_factor/hyperoptimize.py
+++ workflows/scaling_factor/hyperoptimize.py
@@ -145,7 +145,7 @@ def main(args):
                                 mode="min",
                                 points_to_evaluate=[initial_best_config])
 
-    search_alg = ConcurrencyLimiter(search_alg, max_concurrent=4)
+    search_alg = ConcurrencyLimiter(search_alg, max_concurrent=1)
 
     logger.info("Initializing ray Trainable")
     data_train = np.load(
@@ -160,10 +160,10 @@ def main(args):
     else:
         time_budget_s = None
 
-    if args.gpus <= 1:
-        num_cpus_per_trial = args.cpus
-    else:
-        num_cpus_per_trial = 2
+    # if args.gpus <= 1:1
+    #     num_cpus_per_trial = args.cpus
+    # else:
+    #     num_cpus_per_trial = 2
 
     trainer = Trainable(args.root, args.bs, args.lr, args.save_dir,
                         args.epochs, args.n_images, args.v_images,
@@ -178,7 +178,7 @@ def main(args):
         search_alg=search_alg,
         scheduler=scheduler,
         raise_on_failed_trial=False,
-        resources_per_trial={"cpu": num_cpus_per_trial, "gpu": 1},
+        resources_per_trial={"cpu": args.cpus, "gpu": args.gpus},
         resume=args.resume,
         local_dir=args.save_dir,
         log_to_file=True,
