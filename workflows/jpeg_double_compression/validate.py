f6bb01f5eb43b4038c21bb17dee3b0b88fb57f8d
Marcelo Sandoval-Castañeda <marcelo.sc@nyu.edu>
2021-05-20 10:43:13 +0400

    Check labels

--- workflows/jpeg_double_compression/validate.py
+++ workflows/jpeg_double_compression/validate.py
@@ -67,7 +67,7 @@ def validate(model, data, batch_size, cache, num_runs):
                     logits = tf.convert_to_tensor([model(images, training=False) for _ in range(len(num_runs))])
 
                 predictions = tf.squeeze(get_pred(logits))
-                print(predictions.shape, labels.shape)
+                print(predictions, labels)
             accuracies[qf1_ind, qf2_ind] += np.sum(predictions == labels)
 
             print(accuracies[qf1_ind, qf2_ind])
