c7c94e9c7d51fd7561fcbb155c21785093369250
mittalgovind <mittal@nyu.edu>
2021-05-24 10:32:53 -0400

    fix verbose

--- workflows/jpeg_double_compression/validate.py
+++ workflows/jpeg_double_compression/validate.py
@@ -48,8 +48,7 @@ def validate(model, data, batch_size, cache):
 
     for QF1, QF2 in progress_bar(product(q_factors, repeat=2)):
         QF1, QF2 = int(QF1), int(QF2)
-        for batch_id in range(n_batches):
-            batch = data.next_validation_batch(batch_id, batch_size)
+        for batch in data.get_validation_generator(batch_size=batch_size):
             images, labels = data.preprocess_batch(batch, QF1=QF1, QF2=QF2)
 
             # TODO (Marcelo) Add validation for MC dropout
