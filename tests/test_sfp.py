4c2f5d0997aae49b271506e1bde1c8214bb2edf0
Marcelo Sandoval-Castañeda <marcelo.sc@nyu.edu>
2021-06-02 17:16:32 +0400

    test

--- tests/test_sfp.py
+++ tests/test_sfp.py
@@ -68,8 +68,8 @@ def test_dataset(dataset_nojpeg, dataset_jpeg):
     for data in [dataset_nojpeg, dataset_jpeg]:
         for images, labels in data.get_validation_generator(sf=SF):
             print(type(images), type(labels))
-            # assert tf.is_tensor(images)
-            # assert tf.is_tensor(labels)
+            assert tf.is_tensor(images)
+            assert tf.is_tensor(labels)
 
             assert images.shape[0] == labels.shape[0]
             assert images.shape[1] == int(SF * PATCH_SIZE)
