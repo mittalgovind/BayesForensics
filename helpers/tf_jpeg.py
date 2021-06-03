a2c138df6280945a353a7b2451bd37e7d84ac546
Marcelo Sandoval-Castañeda <marcelo.sc@nyu.edu>
2021-06-03 13:07:46 +0400

    Test convert to tensor

--- helpers/tf_jpeg.py
+++ helpers/tf_jpeg.py
@@ -76,21 +76,17 @@ class TFJPEG(JPEG):
     def compress_batch(batch, quality, subsampling="4:4:4"):
         batch_j = tf.zeros((0, *batch.shape[1:]))
         for r in range(batch.shape[0]):
-            '''
             s = io.BytesIO()
             imageio.imsave(
                 s,
-                tf.squeeze(tf.cast((255 * batch[r]), tf.uint8)),
+                tf.squeeze(tf.cast((255 * batch[r]), tf.uint8)).numpy(),
                 format="jpg",
                 quality=quality,
                 subsampling=subsampling,
             )
             image_compressed = imageio.imread(s.getvalue())
-            '''
-            image_compressed = tf.io.encode_jpeg(
-                tf.squeeze(tf.cast((255 * batch[r]), tf.uint8)),
-                quality=quality,
-            )
+            image_compressed = tf.convert_to_tensor(image_compressed)
+
             batch_j = tf.concat((batch_j, tf.divide(image_compressed, 255)))
 
         return batch_j
