fa6a1759f63cad448b309620600780d90d46241e
Marcelo Sandoval-Castañeda <marcelo.sc@nyu.edu>
2021-06-03 13:14:59 +0400

    Test tf.make_ndarray

--- helpers/tf_jpeg.py
+++ helpers/tf_jpeg.py
@@ -72,14 +72,14 @@ class TFJPEG(JPEG):
         return t
 
     @staticmethod
-    # @tf.function(experimental_compile=True)
+    @tf.function(experimental_compile=True)
     def compress_batch(batch, quality, subsampling="4:4:4"):
         batch_j = tf.zeros((0, *batch.shape[1:]))
         for r in range(batch.shape[0]):
             s = io.BytesIO()
             imageio.imsave(
                 s,
-                tf.squeeze(tf.cast((255 * batch[r]), tf.uint8)).numpy(),
+                tf.make_ndarray(tf.squeeze(tf.cast((255 * batch[r]), tf.uint8))),
                 format="jpg",
                 quality=quality,
                 subsampling=subsampling,
