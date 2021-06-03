253884a6cdf0687bdae71cb035efd1447a4deb3a
Marcelo Sandoval-Castañeda <marcelo.sc@nyu.edu>
2021-06-03 13:55:27 +0400

    Test encode/decode

--- helpers/tf_jpeg.py
+++ helpers/tf_jpeg.py
@@ -76,6 +76,7 @@ class TFJPEG(JPEG):
     def compress_batch(batch, quality, subsampling="4:4:4"):
         batch_j = tf.zeros((0, *batch.shape[1:]))
         for r in range(batch.shape[0]):
+            """
             s = io.BytesIO()
             imageio.imsave(
                 s,
@@ -86,6 +87,12 @@ class TFJPEG(JPEG):
             )
             image_compressed = imageio.imread(s.getvalue())
             image_compressed = tf.convert_to_tensor(image_compressed)
+            """
+            image_compressed = tf.io.encode_jpeg(
+                tf.squeeze(tf.cast((255 * batch[r]), tf.uint8)),
+                quality=quality,
+            )
+            image_compressed = tf.io.decode_jpeg(image_compressed)
 
             batch_j = tf.concat((batch_j, tf.divide(image_compressed, 255)))
 
