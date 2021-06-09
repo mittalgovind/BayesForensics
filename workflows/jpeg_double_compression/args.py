643d618ffbf32fc065129188d171fd5721526c4c
mittalgovind <mittal@nyu.edu>
2021-06-09 14:59:40 -0400

    fixing jpeg ..

--- workflows/jpeg_double_compression/args.py
+++ workflows/jpeg_double_compression/args.py
@@ -130,7 +130,7 @@ def parse_args():
     parser.add_argument(
         "--data-dir",
         type=str,
-        default="/home/govind/Workspace/neural-imaging-dev/data/rgb/native12k",
+        default=None,
         help="Data directory for getting images from.",
     )
     parser.add_argument(
