ff48028db3386cd019db9d1881531aafbcdf01b1
mittalgovind <mittal@nyu.edu>
2021-06-16 14:09:50 -0400

    fixing flipout

--- workflows/scaling_factor/validate.py
+++ workflows/scaling_factor/validate.py
@@ -63,7 +63,7 @@ def validate(model, data, batch_size, cache, uncertainty_method, num_runs=50):
                             images, training=False) / model.temperature).numpy()
 
                     else:
-                        logits[i: i + batch_size] = np.array(
+                        logits[:, i: i + batch_size] = np.array(
                             [model(images, training=False) / model.temperature
                              for _ in range(num_runs)])
 
