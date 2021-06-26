fix temp scaling

helpers/uncertainty.py
@@ -22,9 +22,9 @@ def get_pred(logits):
     # logits.shape = (num_runs, batch_size, num_classes)
     # make it batch_first
     # (batch_size, num_runs, num_classes)
-    logits = np.transpose(logits, (1, 0, 2))
+    logits = tf.transpose(logits, (1, 0, 2))
     # (batch_size, num_runs)
-    pred_per_run = np.argmax(logits, axis=-1)
+    pred_per_run = tf.math.argmax(logits, axis=-1)
     # (batch_size, )
     batch_pred = mode(pred_per_run, axis=1)[0]
     return batch_pred

models/bayes/temp_scaling.py
@@ -16,6 +16,7 @@ from tensorflow_probability.python.internal import dtype_util
 import numpy as np
 from loguru import logger
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 # Internal libraries
 
@@ -23,14 +24,30 @@ import matplotlib.pyplot as plt
 class TemperatureScaling(ABC):
     """Decorator for wrapping a TensorFlow model with temperature scaling."""
 
-    def set_temp(self, data, save_dir=None, epochs=100, lr=1e-4):
+    def calibrate(self, epochs, data, num_classes, lr, loss):
+        opt = tf.optimizers.Adam(learning_rate=lr)
+        for _ in tqdm(range(epochs)):
+            if loss < 0.01 or self.temperature < 0.1:
+                break
+            for images, labels in data.get_calibration_generator():
+                logits = tf.cast(self._model(images, training=False),
+                                 tf.float32)
+                labels = tf.cast(labels, tf.int32)
+                with tf.GradientTape() as tape:
+                    tape.watch(self.temperature)
+                    loss = tfp.stats.expected_calibration_error(num_classes,
+                                                                logits / self.temperature,
+                                                                labels)
+                grads = [tape.gradient(loss, self.temperature)]
+                opt.apply_gradients(zip(grads, [self.temperature]))
+
+    def set_temp(self, data, save_dir=None, epochs=5, lr=5e-4):
         """Use validation dataset to calibrate the model."""
         self.temperature = tf.Variable(1, trainable=True, dtype=tf.float32)
         logits_list = []
         labels_list = []
         nll_loss = tf.keras.losses.SparseCategoricalCrossentropy(
             from_logits=True)
-        opt = tf.optimizers.Adam(learning_rate=lr)
 
         # Before training
         for images, labels in data.get_calibration_generator():
@@ -48,24 +65,11 @@ class TemperatureScaling(ABC):
         init_nll_loss = nll_loss(init_labels, init_logits)
         init_ece_loss, init_acc, init_conf = self.expected_calibration_error(
             num_classes, init_logits, init_labels, return_conf=True)
-        self.plot_conf(init_ece_loss, init_acc, init_conf, save_dir, "init")
+        self.plot_conf(init_ece_loss, init_acc, init_conf, title="init")
 
         # train to find temperature
         loss = init_ece_loss
-        for epoch in range(epochs):
-            if loss < 0.01 or self.temperature < 0.1:
-                break
-            for images, labels in data.get_calibration_generator():
-                logits = tf.cast(self._model(images, training=False),
-                                 tf.float32)
-                labels = tf.cast(labels, tf.int32)
-                with tf.GradientTape() as tape:
-                    tape.watch(self.temperature)
-                    loss = tfp.stats.expected_calibration_error(num_classes,
-                                                                logits / self.temperature,
-                                                                labels)
-                grads = [tape.gradient(loss, self.temperature)]
-                opt.apply_gradients(zip(grads, [self.temperature]))
+        self.calibrate(epochs, data, num_classes, lr, loss)
 
         final_nll_loss = nll_loss(init_labels, init_logits / self.temperature)
         final_ece_loss, final_acc, final_conf = self.expected_calibration_error(
@@ -203,7 +207,7 @@ class TemperatureScaling(ABC):
     def plot_conf(ece, acc, conf, save_dir=None, title="init"):
         fig, ax = plt.subplots(1, 1, figsize=(10, 8))
         ax.plot([0, 1], [0, 1], "k--")
-        ax.plot(conf, acc, marker=".")
+        ax.bar(conf, acc)
         ax.set_xlabel(r"confidence")
         ax.set_ylabel(r"accuracy")
         ax.set_xticks((np.arange(0, 1.1, step=0.2)))

workflows/scaling_factor/dataset.py
@@ -179,7 +179,7 @@ class ScalingFactorDataset(Dataset):
         for m, method in enumerate(self.test_methods):
             if self.random_method:
                 self.sampling_method = method
-            for s, sf in enumerate(self.classes[:-1]):
+            for s, sf in enumerate(tf.data.Dataset.from_tensor_slices(self.classes[:-1])):
                 for batch in self.data["calibration"]["y"]:
                     yield self.preprocess_batch(batch, training=False, sf=sf)
 

workflows/scaling_factor/validate.py
@@ -70,6 +70,7 @@ def validate(model, data, batch_size, cache, uncertainty_method,
                     else:
                         logit = [model(images, training=False) / model.temperature
                                  for _ in range(num_runs)]
+                        logit = tf.reshape(logit, (-1, data.n_classes))
                         logits = tf.tensor_scatter_nd_update(logits,
                                     [[m, s, r, i + k] for r in range(num_runs)
                                      for k in range(batch_size)], logit)
@@ -77,14 +78,13 @@ def validate(model, data, batch_size, cache, uncertainty_method,
                     i += batch_size
 
                 if uncertainty_method == "vanilla":
-                    predictions = logits.argmax(axis=-1)
+                    predictions = tf.math.argmax(logits, axis=-1)
                 else:
                     predictions = np.squeeze(get_pred(logits))
 
                 labels, counts = np.unique(predictions, return_counts=True)
-                for label, count in zip(labels, counts):
-                    conf_matrix = tf.tensor_scatter_nd_add(
-                        conf_matrix, [[m, s, label]], [count])
+                conf_matrix = tf.tensor_scatter_nd_add(
+                        conf_matrix, [[m, s, label] for label in labels], counts)
 
                 pbar.update(1)
 
@@ -97,7 +97,7 @@ def validate(model, data, batch_size, cache, uncertainty_method,
     return tests_summary, conf_matrix
 
 
-@tf.function
+# @tf.function
 def distributed_validate(model, data, batch_size, cache, uncertainty_method,
                          test_classes, strategy=None, num_runs=50):
     if strategy: