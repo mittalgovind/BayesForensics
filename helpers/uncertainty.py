51a8115034a1667be1d3bfdd7733eb9a3a8c2a9b
mittalgovind <mittal@nyu.edu>
2021-07-12 15:53:07 -0400

    compile uncertainty.py

--- helpers/uncertainty.py
+++ helpers/uncertainty.py
@@ -18,7 +18,6 @@ MAX = int(2e16)
 MIN = -int(2e16)
 
 
-@tf.function(experimental_compile=True)
 def get_pred(logits):
     # logits.shape = (num_runs, batch_size, num_classes)
     # make it batch_first
@@ -31,7 +30,6 @@ def get_pred(logits):
     return batch_pred
 
 
-@tf.function(experimental_compile=True)
 def get_probs_passes_logits(logits):
     # logits.shape = (num_runs, batch_size, num_classes)
     # make it batch_first
@@ -42,7 +40,6 @@ def get_probs_passes_logits(logits):
     return probs, n_passes, logits
 
 
-@tf.function(experimental_compile=True)
 def variation_ratio(logits):
     probs, n_passes, _ = get_probs_passes_logits(logits)
     means = np.array([[np.sum(c) / n_passes for c in run.T] for run in probs])
@@ -50,7 +47,6 @@ def variation_ratio(logits):
     return var_ratio
 
 
-@tf.function(experimental_compile=True)
 def predictive_entropy(logits):
     probs, n_passes, _ = get_probs_passes_logits(logits)
     means = np.array([[np.sum(c) / n_passes for c in run.T] for run in probs])
@@ -61,7 +57,6 @@ def predictive_entropy(logits):
     return pred_ent
 
 
-@tf.function(experimental_compile=True)
 def mutual_information(logits):
     probs, n_passes, logits = get_probs_passes_logits(logits)
     pred_ent = predictive_entropy(logits)
